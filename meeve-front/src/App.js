c9c9bf3a1a4efad6f28218622826f3cdf5fe7d73
ayman0kallel <ayman0kallel@gmail.com>
2023-09-26 14:52:07 +0200

    commit

--- meeve-front/src/App.js
+++ meeve-front/src/App.js
@@ -1,8 +1,10 @@
 import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <SignIn/>
     </div>
   );
---
+++ meeve-front/src/components/SignUp.js
@@ -0,0 +1,14 @@
+// SignUp.js
+import React from 'react';
+
+function SignUp() {
+  return (
+    <div>
+      {/* Your sign-up form and content */}
+      <h2>Sign Up</h2>
+      {/* Add your sign-up form fields and logic here */}
+    </div>
+  );
+}
+
+export default SignUp;
