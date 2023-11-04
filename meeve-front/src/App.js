070393c2f1c09532858bda922b0fbcd2efd3301d
Margavivas <marga.imdesigner@gmail.com>
2023-11-04 13:18:19 +0100

    style(profile)

--- meeve-front/src/App.js
+++ meeve-front/src/App.js
@@ -1,6 +1,7 @@
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import HomePage from "../src/pages/HomePage"
+import Profile from "../src/pages/Profile"
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<SignIn />} />
           <Route path="/Signup" element={<SignUp/>}/>
           <Route path="/HomePage" element={<HomePage/>}/>
+          <Route path="/Profile" element={<Profile/>}/>
         </Routes>
       </Router>
     </div>
--- meeve-front/src/pages/Profile.js
+++ meeve-front/src/pages/Profile.js
@@ -7,6 +7,7 @@ import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import GroupIcon from '@mui/icons-material/Group';
 import Layout from '../Layout';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ProfileStyle from "../style/pages/Profile.css"
 
 const theme = createTheme({
   typography: {
@@ -35,94 +36,21 @@ const theme = createTheme({
 });
 
 const styles = {
-    container: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      textAlign: 'center',
-      padding: '20px',
-    },
-    sportsContainer: {
-      width: '90%',
-      margin: '20px auto 0', 
-      backgroundColor: '#F5F5F5',
-      borderRadius: '10px',
-      boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
-      padding: '10px',
-      display: 'flex',
-      flexDirection: 'column',
-    },
+
     avatar: {
       width: '100px',
       height: '100px',
       marginBottom: '10px',
       border: '2px solid #1ccf90',
     },
-    sportList: {
-      listStyleType: 'none',
-      padding: '0',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    sportItem: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      color: '#333',
-      backgroundColor: '#E5E5E5',
-      borderRadius: '5px',
-      padding: '10px',
-      margin: '5px',
-      flex: '1',
-    },
-    sportIcon: {
-      fontSize: '30px',
-    },
-    sportName: {
-      fontSize: '14px',
-      marginTop: '5px',
-    },
-    friendsCountContainer: {
-      display: 'flex',
-      alignItems: 'center',
-      backgroundColor: '#E5E5E5',
-      borderRadius: '5px',
-      padding: '10px',
-      margin: '5px',
-    },
-    friendsIcon: {
-      fontSize: '30px',
-      marginRight: '10px',
-    },
-    friendsCount: {
-      fontSize: '14px',
-    },
-  biographySection: {
-    width: '90%',
-    marginTop: '20px',
-    backgroundColor: '#F5F5F5',
-    borderRadius: '10px',
-    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
-    padding: '10px',
-    textAlign: 'left', 
-  },
-  biographyTitle: {
-    fontSize: '18px', 
-    fontWeight: 'bold', 
-  },
-  biographyText: {
-    fontSize: '14px', 
-    lineHeight: '1.2', 
-    
-  },
+   
 };
 
 const Profile = () => {
   const userProfile = {
     username: 'User',
     profileImage: 'URL',
+    favoriteGym: 'Basic Fit',
     sports: ['Footbal', 'Basket', 'Gym'],
     friendsCount: 50,
     biography:
@@ -132,7 +60,7 @@ const Profile = () => {
   return (
     <Layout>
       <ThemeProvider theme={theme}>
-        <div style={styles.container}>
+        <div className="ProfileContainer">
           <Avatar
             alt={userProfile.username}
             src={userProfile.profileImage}
@@ -140,41 +68,41 @@ const Profile = () => {
           />
           <Typography variant="h5">{userProfile.username}</Typography>
 
-          <div style={styles.sportsContainer}>
+          <div className="sportsContainer" >
             <Typography variant="subtitle1">Sport pratiqués</Typography>
-            <ul style={styles.sportList}>
+            <ul className='sportList'>
               {userProfile.sports.map((sport, index) => (
-                <li key={index} style={styles.sportItem}>
+                <li key={index} className='sportItem'>
                   <div>
                     {sport === 'Footbal' && (
-                      <SportsSoccerIcon style={styles.sportIcon} />
+                      <SportsSoccerIcon className= "sportIcon"  />
                     )}
                     {sport === 'Basket' && (
-                      <SportsBasketballIcon style={styles.sportIcon} />
+                      <SportsBasketballIcon className= "sportIcon"/>
                     )}
                     {sport === 'Gym' && (
-                      <FitnessCenterIcon style={styles.sportIcon} />
+                      <FitnessCenterIcon className= "sportIcon" />
                     )}
                   </div>
-                  <Typography variant="body2" style={styles.sportName}>
+                  <Typography variant="body2" className="sportName">
                     {sport}
                   </Typography>
                 </li>
               ))}
             </ul>
-            <div style={styles.friendsCountContainer}>
-              <GroupIcon style={styles.friendsIcon} />
-              <Typography variant="body2" style={styles.friendsCount}>
+            <div className='friendsCountContainer'>
+              <GroupIcon className="friendsIcon"  />
+              <Typography variant="body2" className="friendsCount" >
                 Amies {userProfile.friendsCount}
               </Typography>
             </div>
           </div>
 
-          <section style={styles.biographySection}>
-            <Typography variant="h6" style={styles.biographyTitle}>
+          <section  className='biographySection' >
+            <Typography variant="h6" className='biographyTitle' >
               Biography
             </Typography>
-            <Typography variant="body2" style={styles.biographyText}>
+            <Typography variant="body2" className='biographyText'>
               {userProfile.biography}
             </Typography>
           </section>
