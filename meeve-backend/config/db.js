3166a1ba25d925b567223eacc49733588162b970
ayman0kallel <ayman0kallel@gmail.com>
2023-12-03 21:32:17 +0100

    fix database

--- meeve-backend/config/db.js
+++ meeve-backend/config/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'meeve',
+  host: 'sql11.freesqldatabase.com',
+  user: 'sql11667222',
+  password: 'FnwwLILQXL',
+  database: 'sql11667222',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
