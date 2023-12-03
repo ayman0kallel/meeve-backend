c858747fd5d09623bd075ae2ecdf6726020033bc
ayman0kallel <ayman0kallel@gmail.com>
2023-12-03 22:32:56 +0100

    fix cors

--- meeve-backend/index.js
+++ meeve-backend/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import usersRoutes from './routes/userRoutes.js'
 import db from './config/db.js'
 import { createUserTable } from './models/userModel.js'; // Import the createUserTable function correctly
 
 const app = express();
-const cors = require('cors');
 const PORT = 5000;
 
 // Call the createUserTable function to create the 'users' table
