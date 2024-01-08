1c0762fec50b9b26fc2c4b28ae3da30f6fe84d8e
ayman0kallel <ayman0kallel@gmail.com>
2024-01-08 16:53:47 +0100

    fix new database

--- meeve-backend/config/db.js
+++ meeve-backend/config/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'sql11.freesqldatabase.com',
-  user: 'sql11673668',
-  password: 'RFYkvNEuMZ',
-  database: 'sql11673668',
+  host: 'localhost',
+  user: 'id21614262_meeve',
+  password: 'MeeveCIM1@',
+  database: 'id21614262_meevedb',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
